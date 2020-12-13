8155019af3ac562b671f1821b345e78d5214ec9c
GerardoTec <69990146+GerardoTec@users.noreply.github.com>
2020-12-13 17:35:12 -0600

    ya ahora si ya quedo, le falta css

--- src/app/pages/mensajeria/mensajeria.page.ts
+++ src/app/pages/mensajeria/mensajeria.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mensajeria',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MensajeriaPage implements OnInit {
 
-  constructor() { }
+  constructor( private navCrtl : NavController) { }
 
   ngOnInit() {
+  }
+
+  navcuenta(){
+    this.navCrtl.navigateForward('/cuenta');
   }
 
 }
